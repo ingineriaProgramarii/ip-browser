c407530446d2f650967a080733a5db4d601be09b
teoflavian <theodorhanu@gmail.com>
2014-05-11 16:59:14 +0300

    database

---
+++ backend/src/cache/Database.java
@@ -0,0 +1,32 @@
+package cache;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+    public Connection conn;
+    public static Database instance = null;
+
+    private Database() {
+        String url = "https://students.info.uaic.ro/db/";
+        String dbName = "ipCache";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "ipCache";
+        String password = "j5QQ9DcQ7h";
+        try {
+            Class.forName( driver ).newInstance();
+            this.conn = DriverManager.getConnection( url + dbName, userName, password );
+        }
+        catch( Exception e ) {
+            e.printStackTrace();
+        }
+    }
+
+    public Database getInstance() {
+        if( instance == null ) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+}
