26212bf1e2972835d1cb2ab7a57267298ddaa54e
DanUnguru <robertunguru@gmail.com>
2014-05-11 18:28:21 +0300

    Adaugat constructor la HistoryItem.

--- backend/src/cache/Cache.java
+++ backend/src/cache/Cache.java
@@ -1,9 +1,6 @@
 package cache;
 
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -14,8 +11,6 @@ public class Cache {
 
     public ArrayList<HistoryItem> History;
 
-    private Connection dbConnection;
-
     public void addCookie( String name, String domain, String value, Date expireDate, String path, Boolean secure ) {
     }
 
@@ -24,29 +19,7 @@ public class Cache {
     }
 
     public ArrayList<Cookie> getDomainCookies( String domain ) {
-        this.cookies = new ArrayList<Cookie>();
-        this.dbConnection = Database.getInstance().getConn();
-        String sql = "SELECT * FROM cookies WHERE domain = '" + domain + "' AND expireDate < DATETIME('now');";
-        java.sql.Statement stmt = null;
-        try {
-            stmt = dbConnection.createStatement();
-            ResultSet results = stmt.executeQuery( sql );
-            while( results.next() ) {
-                cookies.add( new Cookie(
-                        domain,
-                        results.getString( "name" ),
-                        results.getString( "value" ),
-                        results.getDate( "expireDate" ),
-                        results.getString( "path" ),
-                        results.getBoolean( "secure" ) )
-                           );
-            }
-            results.close();
-        }
-        catch( SQLException e ) {
-            e.printStackTrace();
-        }
-        return this.cookies;
+        return null;
     }
 
     public Cache() {
--- backend/src/cache/HistoryItem.java
+++ backend/src/cache/HistoryItem.java
@@ -9,13 +9,16 @@ public class HistoryItem {
 
     private String url;
 
+    private String tag;
+
     public HistoryItem() {
     }
 
-    public HistoryItem(String _name, Date _lastAccess, String _url) {
+    public HistoryItem(String _name, Date _lastAccess, String _url, String _tag) {
         name = _name;
         lastAccess = _lastAccess;
         url = _url;
+        tag = _tag;
     }
 
     public void delete() {
@@ -43,5 +46,13 @@ public class HistoryItem {
 
     public void setUrl( String url ) {
         this.url = url;
+    }
+
+    public void setTag( String _tag) {
+        tag = _tag;
+    }
+
+    public String getTag() {
+        return tag;
     }
 }
